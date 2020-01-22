60dfc04f5392c1bc8ac5733b9e1d4c4db0c4aced
junhongGenesislab <unique3801@gmail.com>
2020-01-23 07:32:13 +0900

    add subscriber app

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Clients(models.Model):
+  email = models.EmailField() 
+  register_date = models.DateField(auto_now_add=True)
+  # activation 이메일 보내기 
+  activate = models.BooleanField()
+  # 비활성화 링크 포함하기 
+  subscription = models.BooleanField()
