eb9da319b4c14beaa09c7bfebefd7592f2c208e9
junhongGenesislab <unique3801@gmail.com>
2020-01-23 07:33:41 +0900

    migrate subscriber app

--- pages/models.py
+++ pages/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-
-
-class Clients(models.Model):
-  email = models.EmailField() 
-  register_date = models.DateField(auto_now_add=True)
-  # activation 이메일 보내기 
-  activate = models.BooleanField()
-  # 비활성화 링크 포함하기 
-  subscription = models.BooleanField()
